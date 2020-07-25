5795effe5adb62ebb7a064df707e80ceb5570b40
Erik Norfelt <enorfelt@outlook.com>
2020-07-25 22:43:48 +0200

    .

--- valve.py
+++ valve.py
@@ -7,7 +7,7 @@ class Valve:
 
   def __init__(self):
     self.i2c = busio.I2C(board.SCL, board.SDA)
-    self.dac = adafruit_mcp4725.MCP4725(i2c)
+    self.dac = adafruit_mcp4725.MCP4725(self.i2c)
     print("init normalized value to {}".format(0.0))
     self.dac.noramlized_value = 0.0
 
