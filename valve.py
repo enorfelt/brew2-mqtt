b6e0c79b24cb83609ef28678708cfa4112f88f7c
Erik Norfelt <enorfelt@outlook.com>
2020-07-26 08:01:56 +0200

    .

--- valve.py
+++ valve.py
@@ -4,14 +4,15 @@ import adafruit_mcp4725
 
 
 class Valve:
-    def __init__(self):
-        self.i2c = busio.I2C(board.SCL, board.SDA)
-        self.dac = adafruit_mcp4725.MCP4725(self.i2c)
+    i2c = busio.I2C(board.SCL, board.SDA)
+    dac = adafruit_mcp4725.MCP4725(i2c)
+
+    def __init__(self):        
         print("init normalized value to {}".format(0.0))
-        self.dac.noramlized_value = 0.0
+        Valve.dac.noramlized_value = 0.0
 
     def setPercentageOpen(self, normalized):
         value = float(normalized)
         print("set normalized value to {}".format(value))
         print("Yes") if value == 0.5 else print("No")
-        self.dac.noramlized_value = value
+        Valve.dac.noramlized_value = value
